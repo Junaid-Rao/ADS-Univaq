a98d19a3d378fcb083e60b85b3a67bff53da7076
Junaid <junaidaslam.muet@gmail.com>
2026-03-27 02:09:49 -0700

    Create integerSort.java

---
+++ integerSort.java
@@ -0,0 +1,33 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] a = {43,5,12,54,12,1,65,76};
+	    int[] b = integerSort(a);
+		print(b);
+	}
+	public static int[] integerSort(int[] arr){
+	    // Find the maximium element as k 
+	    
+	    // Make the array of k elements
+	    
+	    // Initialize them with 0
+	    
+	    // Loop through unsorted array and increase counters for indexes for which the element is Present
+	    
+	    // Based on New Integer Count Array, reassign the original array with new elements.
+	    
+	    // Into the worst case, when k ~ Infinity
+	    
+	    // Into the best case, when k ~ n. Hence complexity would be O(n)
+	    
+	    
+        return arr;
+    }
+    public static void print(int[] arr){
+        for (int i=0; i<arr.length; i++){
+        	System.out.print(arr[i] + ", ");
+        }
+        System.out.println();
+    }
+    
+}
