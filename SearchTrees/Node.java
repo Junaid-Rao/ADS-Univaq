1c0b9c7efedf3697705e3d6ff8106576d37c5fcb
Junaid <junaidaslam.muet@gmail.com>
2026-04-02 04:22:12 -0700

    Create Node class for binary tree structure

---
+++ SearchTrees/Node.java
@@ -0,0 +1,15 @@
+public class Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(){
+        data = 0;
+        left = null;
+        right = null;
+    }
+    public Node(int d){
+        data = d;
+        left = null;
+        right = null;
+    }
+}
