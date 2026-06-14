893c3adb095f2cce1436e3d0f556bd047b11cbf5
Junaid <junaidaslam.muet@gmail.com>
2026-06-14 05:39:34 -0700

    Update quickSort.java

--- quickSort.java
+++ quickSort.java
@@ -1,49 +1,66 @@
-public class Main
-{
-	public static void main(String[] args) {
-	    int[] a = {45,5,42,34,32,31,27,26};
-        System.out.println("QuickSort Steps:");
-	    quickSort(a, 0, a.length-1);
-        System.out.println("Final Sorted Array:");
-		print(a);
-	}
-	public static void quickSort(int[] arr, int low, int high){
-	    if(low<high){
-	        int pivot = partition(arr, low, high);
-	        quickSort(arr, low, pivot-1);
-	        quickSort(arr, pivot+1, high);
-	    }
-	}
-    public static int partition(int[] arr, int low, int high){
-	    int pivot = arr[high];
-	    int i = low-1;
-	    
-	    for(int j = low; j<high ; j++){
-	        if(arr[j] <= pivot){
-	            i++;
-	            int temp = arr[j];
-	            arr[j] = arr[i];
-	            arr[i] = temp;
-	        }
-	        print(arr, low, high);
-	    }
-	    
-	    int temp = arr[i+1];
-	    arr[i+1]=arr[high];
-	    arr[high] = temp;
-	    return i+1;
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {45, 5, 42, 34, 32, 31, 27, 26};
+
+        System.out.println("Original Array:");
+        print(arr);
+
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("\nFinal Sorted Array:");
+        print(arr);
     }
-    public static void print(int[] arr){
-        for (int i=0; i<arr.length; i++){
-        	System.out.print(arr[i] + ", ");
+
+    public static void quickSort(int[] arr, int low, int high) {
+
+        if (low < high) {
+
+            int pivotIndex = partition(arr, low, high);
+
+            System.out.print("After partition (pivot = "
+                    + arr[pivotIndex] + "): ");
+            print(arr);
+
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
         }
-        System.out.println();
     }
-    
-    public static void print(int[] arr, int a, int b){
-        for (int i=a; i<=b; i++){
-        	System.out.print(arr[i] + ", ");
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+
+            if (arr[j] <= pivot) {
+
+                i++;
+
+                if (i != j) {
+                    swap(arr, i, j);
+                }
+            }
         }
+
+        swap(arr, i + 1, high);
+
+        return i + 1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void print(int[] arr) {
+
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+
         System.out.println();
     }
 }
