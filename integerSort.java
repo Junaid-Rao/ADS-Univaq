59c5937acae392adc3858b8ef0424f9dae937b90
Junaid <junaidaslam.muet@gmail.com>
2026-06-14 06:13:05 -0700

    Update integerSort.java

--- integerSort.java
+++ integerSort.java
@@ -1,25 +1,48 @@
 public class Main
 {
 	public static void main(String[] args) {
-	    int[] a = {43,5,12,54,12,1,65,76};
+	    int[] a = {3,5,1,4,2,1,6,7};
 	    int[] b = integerSort(a);
 		print(b);
 	}
 	public static int[] integerSort(int[] arr){
-	    // Find the maximium element as k 
-	    
+	    if (arr == null || arr.length == 0) {
+            return arr;
+        }
+
+        // Find maximum element (k)
+        int max = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        
 	    // Make the array of k elements
-	    
-	    // Initialize them with 0
+	    int[] count = new int[max + 1];
+
+	    // Initialize them with 0 : Java Automatically does this..
 	    
 	    // Loop through unsorted array and increase counters for indexes for which the element is Present
-	    
+	    for (int num : arr) {
+            count[num]++;
+        }
+        
 	    // Based on New Integer Count Array, reassign the original array with new elements.
+        int index = 0;
+        for (int value = 0; value <= max; value++) {
+            while (count[value] > 0) {
+                arr[index++] = value;
+                count[value]--;
+            }
+        }
+        
+        return arr;
+        
 	    
-	    // Into the worst case, when k ~ Infinity
-	    
-	    // Into the best case, when k ~ n. Hence complexity would be O(n)
-	    
+	    // Time complexity would be O(k+n). Into the best case, when k ~ n. 
+	    // Space complexity would be of Count Array = O(k)
 	    
         return arr;
     }
